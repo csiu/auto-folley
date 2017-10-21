968ef4164f1d50e959cac20871269e3c7abf61e0
csiu <celia.sy.siu@gmail.com>
2017-10-21 10:06:12 -0700

    generalize

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ import argparse
 import spacy
 import pysrt
 import nltk
-# import sys; sys.path.append("/Users/csiu/repo/auto-folley/src")
 import mix
 
 
@@ -11,10 +10,11 @@ usage = """
 
 spacy_model = 'en'
 sound_track = "data/sound/sad-trombone-73581_634166-lq.wav"
-subtitle_track = 'data/subtitles/taxi_driver.srt'
-video = "data/videos/taxi_driver_tp_clip.mkv"
-target_word = u'funny'
-resulting_mkv_video = "out.mkv"
+subtitle_track = 'data/casablanca-rEWaqUVac3M.srt'
+video = "data/casablanca-rEWaqUVac3M.mp4"
+target_word = u'sad'
+resulting_mkv_video = "out.mp4"
+threshold = 0.58
 
 def parse_cue(sub):
     hours = sub.end.hours * 3600000
@@ -32,7 +32,7 @@ def main():
     cues = []
     for sub in subs:
         score = target.similarity(nlp(sub.text))
-        if score > 0.70:
+        if score > threshold:
             # print(sub.text, score)
             cues.append(parse_cue(sub))
 
